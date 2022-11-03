3cb1de2629f08783b71fb963a1e289d236307ea5
Martinpasaribu <83689864+Martinpasaribu@users.noreply.github.com>
2022-11-03 19:56:45 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+//  // Merubah teks kedalam id 
+// const judul = document.getElementById('judul');
+// judul.innerHTML = ' ini Web Martin Pasaribu'
+
+
+// // Membuat Elemen html kedalam id  
+// const sectionB = document.getElementById('b');
+// sectionB.innerHTML = '<p class="nice"> Hai saya sudah Berganti</p>'
